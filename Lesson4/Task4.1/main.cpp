b5abce4ce830380ba88c43cb64993d8df6fc43a1
ElenNetology <lepra796@gmail.com>
2024-01-05 15:22:32 +0400

    Task4

---
+++ Lesson4/Task4.1/main.cpp
@@ -0,0 +1,14 @@
+﻿// Task4.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "shape.h"
+#include "line.h"
+#include "transform.h"
+
+int main()
+{
+	Line line(1, 1, 1, 1);
+	std::cout << line.getType();
+	line.scale(2);
+}
