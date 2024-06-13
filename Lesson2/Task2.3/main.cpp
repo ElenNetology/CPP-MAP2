50dddc0fd324c06a4f509d63573dd4c42e32fd7d
ElenNetology <lepra796@gmail.com>
2024-06-13 20:07:36 +0400

    Task3

--- Lesson2/Task2.3/main.cpp
+++ Lesson2/Task2.3/main.cpp
@@ -8,130 +8,96 @@
 
 using namespace std::chrono_literals;
 
-void SetColor(int text, int bg) {
-	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hStdOut, (WORD)((bg << 1) | text));
-}
-
 class Data {
-public:
-	Data(std::vector<int> data) : data{ data } {}
 private:
-	friend void swap1(Data& lhs, Data& rhs);
-	friend void swap2(Data& lhs, Data& rhs);
-	friend void swap3(Data& lhs, Data& rhs);
-	friend void progressBar(Data& lhs, Data& rhs);
+	int value_ = 0;
 
-	std::vector<int> data;
-	std::mutex m;
+public:
+	Data() = default;
+	Data(int value) : value_(value) {}
+
+	std::mutex mutex_;
+
+    int getValue() const
+    {
+        return Data::value_;
+    }
+    void setValue(int value)
+    {
+        Data::value_ = value;
+    }
 };
 
-void swap1(Data& lhs, Data& rhs);
 
-void swap2(Data& lhs, Data& rhs);
+void swapWithLock(Data& d1, Data& d2)
+{
+    std::lock(d1.mutex_, d2.mutex_);
 
-void swap3(Data& lhs, Data& rhs);
+    //swap
+    int tmp = d1.getValue();
+    d1.setValue(d2.getValue());
+    d2.setValue(tmp);
 
-void progressBar(Data& lhs, Data& rhs);
+    std::cout << std::endl << "ThreadId: " << std::this_thread::get_id() << ". После обмена: " __FUNCTION__ << std::endl;
+    std::cout << "value_ = " << d1.getValue() << ";" << std::endl;
+    std::cout << "value_ = " << d2.getValue() << ";" << std::endl;
+
+    d1.mutex_.unlock();
+    d2.mutex_.unlock();
+}
+
+void swapWithScopedLock(Data& d1, Data& d2)
+{
+    std::scoped_lock lock(d1.mutex_, d2.mutex_);
+
+    //swap
+    int tmp = d1.getValue();
+    d1.setValue(d2.getValue());
+    d2.setValue(tmp);
+
+    std::cout << std::endl << "ThreadId: " << std::this_thread::get_id() << ". После обмена: " __FUNCTION__ << std::endl;
+    std::cout << "value_ = " << d1.getValue() << ";" << std::endl;
+    std::cout << "value_ = " << d2.getValue() << ";" << std::endl;
+}
+
+void swapWithUniqueLock(Data& d1, Data& d2)
+{
+    std::unique_lock<std::mutex> lock1(d1.mutex_, std::defer_lock);
+    std::unique_lock<std::mutex> lock2(d2.mutex_, std::defer_lock);
+    lock1.lock();
+    lock2.lock();
+    //swap
+    int tmp = d1.getValue();
+    d1.setValue(d2.getValue());
+    d2.setValue(tmp);
+
+    std::cout << std::endl << "ThreadId: " << std::this_thread::get_id() << ". После обмена: " __FUNCTION__ << std::endl;
+    std::cout << "value_ = " << d1.getValue() << ";" << std::endl;
+    std::cout << "value_ = " << d2.getValue() << ";" << std::endl;
+    lock1.unlock();
+    lock2.unlock();
+}
 
 int main()
 {
-	std::vector<int> data1{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	std::vector<int> data2{ 11, 12, 13, 14, 15, 16, 17, 18, 19 };
-	Data d1(data1), d2(data2);
-	std::thread t1(swap1, std::ref(d1), std::ref(d2));
-	std::thread t2(swap2, std::ref(d1), std::ref(d2));
-	std::thread t3(swap3, std::ref(d1), std::ref(d2));
-	t1.join();
-	t2.join();
-	t3.join();
-	return 0;
-}
-void progressBar(Data& lhs, Data& rhs) {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dis(0, 100);
-	char fillingChar = '#';
-	char leftBoundary = '[';
-	char rightBoundary = ']';
-	int width = 50; 
-	int current = 0; 
-	int total = 100; 
-	int step = total / (lhs.data.size() - 1); 
-	int progress = 0; 
-	std::vector<std::pair<std::string, int>> bar;
-	int previousFill = 0;
-	for (int i = 0; i < lhs.data.size(); ++i) {
-		
-		double percent = (progress * 100) / total;
-		percent = (i == lhs.data.size() - 1 && percent != 100) ? 100 : (progress * 100) / total;
-		
-		int fill = (percent * width) / 100;
-		try {
-			if (dis(gen) % 3 == 0) {
-				throw std::runtime_error("Error");
-			}
-			int tmp = lhs.data.at(i);
-			lhs.data.at(i) = rhs.data.at(i);
-			rhs.data.at(i) = tmp;
-			bar.push_back(std::pair (std::string(fill - previousFill, fillingChar), 3));
-		}
-		catch (const std::exception& e) {
-			bar.push_back(std::pair (std::string(fill - previousFill, fillingChar), 5));
-		}
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "ID main thread: " << std::this_thread::get_id() << "\n";
 
-		SetColor(3, 3);
-		std::cout << "\r" << leftBoundary;
-		for (int j = 0; j < bar.size(); ++j) {
-			SetColor(bar.at(j).second, 0);
-			for (int c = 0; c < bar.at(j).first.size(); ++c) {
-				if (j != bar.size() - 1) {
-					std::this_thread::sleep_for(0ms);
-				}
-				else {
-					std::this_thread::sleep_for(100ms);
-				}
-				std::cout << bar.at(j).first.at(c);
-			}
-		}
-		for (int j = fill; j < width; j++) {
-			SetColor(3, 3);
-			std::cout << " ";
-		}
-		SetColor(3, 3);
-		std::cout << rightBoundary << " " << percent << "%";
-		
-		current += step;
-		progress += step;
-		previousFill = fill;
+    Data d1(55555);
+    Data d2(11111);
 
-	}
-	std::cout << std::endl;
-	SetColor(3, 3);
-}
-void swap1(Data& lhs, Data& rhs)
-{
-	lhs.m.lock();
-	rhs.m.lock();
-	std::cout << "Thread's id: " << std::this_thread::get_id() << '\n';
-	progressBar(lhs, rhs);
-	lhs.m.unlock();
-	rhs.m.unlock();
-}
+    std::cout << "\nДо обмена:\n";
+    std::cout << "value_ = " << d1.getValue() << ";" << std::endl;
+    std::cout << "value_ = " << d2.getValue() << ";" << std::endl;
 
-void swap2(Data& lhs, Data& rhs)
-{
-	std::scoped_lock lock(lhs.m, rhs.m);
-	std::cout << "Thread's id: " << std::this_thread::get_id() << '\n';
-	progressBar(lhs, rhs);
-}
+    std::thread thread1(swapWithLock, std::ref(d1), std::ref(d2));
+    std::thread thread2(swapWithScopedLock, std::ref(d1), std::ref(d2));
+    std::thread thread3(swapWithUniqueLock, std::ref(d1), std::ref(d2));
 
-void swap3(Data& lhs, Data& rhs)
-{
-	std::unique_lock ul1(lhs.m);
-	std::unique_lock ul2(rhs.m);
-	std::cout << "Thread's id: " << std::this_thread::get_id() << '\n';
-	progressBar(lhs, rhs);
-	ul1.unlock();
-	ul2.unlock();
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    //std::cout << "\n\nend of main\n";
 }
